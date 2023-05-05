0cfe42eaf62f134a793df0172b1b9ffbee81348c
Andressa <andressaqmedina@gmail.com>
2023-05-04 21:26:23 -0300

    alteração nos tamanhos e email

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(150, {'Content-Type': 'text/html'});
         res.write('<html><body><h1>Página inicial</h1><p>Bem-vindo à página inicial da aplicação web!</p></body></html>');
         res.end();
   } else if (req.method === 'GET' && req.url === '/sobre') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(150, {'Content-Type': 'text/html'});
         res.write('<html><body><h1>Sobre</h1><p>Esta é a página sobre a aplicação web!</p></body></html>');
         res.end();
   } else if (req.method === 'GET' && req.url === '/contato') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<html><body><h1>Contato</h1><p>Entre em contato conosco através do e-mail contato@aplicacaoweb.com.br</p></body></html>');
+        res.writeHead(150, {'Content-Type': 'text/html'});
+        res.write('<html><body><h1>Contato</h1><p>Entre em contato pelo email nandogarciafacul@gmail.com</p></body></html>');
         res.end();
   } else {  
         res.writeHead(404);
